741d7a94493efdd6d5ab5fb461d96adeaa77ecbc
Kevin Ebunilo <kebunilo@gmail.com>
2026-03-09 16:56:14 -0400

    New Update

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -15,7 +15,8 @@ const ConferenceEvent = () => {
     const dispatch = useDispatch();
     //As the name describes, it calculates the remaning number of seats wihin the venue
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
-
+    // variable pulls infromation and methods from mealsSlice.js
+    const mealsItems = useSelector((state) => state.meals);
     
     const handleToggleItems = () => {
         console.log("handleToggleItems called");
@@ -212,12 +213,27 @@ const ConferenceEvent = () => {
 
                                     <h1>Meals Selection</h1>
                                 </div>
-
+                                {/* Selection box that collects the number of people */}
                                 <div className="input-container venue_selection">
-
+                                    <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
+                                    <input type ="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
+                                        onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                                        min="1"
+                                        />
                                 </div>
                                 <div className="meal_selection">
-
+                                    {mealsItems.map((item,index) => (
+                                        <div className="meals_item" key={index} style={{padding: 15}}>
+                                            <div className="inner">
+                                                <input type="checkbox" id ={`meal_${index}`}
+                                                    checked={item.selected}
+                                                    onChange={() => handleMealSelection(index)}
+                                                    />
+                                                <label htmlFor={`meal_${index}`}> {item.name} </label>                       
+                                            </div>
+                                            <div className="meal_cost">${item.cost}</div>
+                                        </div>
+                                    ))}
                                 </div>
                                 <div className="total_cost">Total Cost: </div>
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-import avReducer from "./avSlice"
+import avReducer from "./avSlice";
+import mealsReducer from "./mealsSlice";
 export default configureStore({
   //Global Reducer that allow all components in the application to use the methods found in the reducer 
   reducer: {
     venue: venueReducer,
     av: avReducer,
+    meal: mealsReducer,
   },
 });
